bf3f77a934b054ffbf17146635221ea5a6c9cb15
김용각 <bw4487@naver.com>
2025-11-30 19:45:33 +0900

    11/30

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const pool = new Pool({
 console.log('PostgreSQL DB 연결 풀 생성됨');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
